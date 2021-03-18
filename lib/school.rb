3cba21a20a2305c4237171bf382a4ab5893ba9f2
Zecharias Mengis <zmengis@gmail.com>
2021-03-18 00:41:22 -0500

    commit tree dbd5396bde4dee5bd1c908c15fd52da87125f8da

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,15 @@
-# code here!
+require 'pry'
+
+class School
+    attr_reader :roster
+
+    def initialize(roster)
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        # binding.pry
+        @roster = @roster[grade]
+        binding.pry
+    end
+end
